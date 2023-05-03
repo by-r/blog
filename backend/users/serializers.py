e20f76048e284517b1923b59f43a8abf12d2aacc
Mohd Razzeq <rzq272@gmail.com>
2023-05-03 21:37:04 +0800

    Add class to override get_token method

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length = 8, write_only = True)
@@ -7,3 +8,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+# subclass from SimpleJWT lib - used to serialize/deserialize the tokens that are used to auth API
+# overrides get_token method from base of TokenObtainPairSerializer class :: get_token - generate JWT that is returned to client when auth req
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user) # invoke get_token method and return JWT that it generates :: JWT modified with username/email fields
+        token['username'] = user.username
+        token['email'] = user.email
+        return token
