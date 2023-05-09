88bcb4675abab1772e9cfa03bd4a88839a9fb005
Mohd Razzeq <rzq272@gmail.com>
2023-05-09 18:20:45 +0800

    Add relevant URLs

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from .views import UsersAPIView
+from .views import UsersAPIView, MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('', UsersAPIView.as_view()),
+    path('login/', MyTokenObtainPairView.as_view()),
+    # The TokenRefreshView view allows us to refresh a JSON Web Token (JWT) by sending a request with a refresh token to the view.
+    path('token/refresh', TokenRefreshView.as_view()),
 ]
