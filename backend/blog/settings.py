2b04b5037c425ea6d31e8add90f9fb645ea9629a
Mohd Razzeq <rzq272@gmail.com>
2023-05-09 20:08:30 +0800

    Typo fix

--- backend/blog/settings.py
+++ backend/blog/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        #"rest_framework_simplejwt.authentication.JWTAuthentication",
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
